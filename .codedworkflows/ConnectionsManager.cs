1c771f4716d0a93dd05cf439f57df6c4d8a51af9
Ruchita Patil <ruchita.patil@thomsonreuters.com>
2024-12-31 15:51:02 +0530

    latest

---
+++ .codedworkflows/ConnectionsManager.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+using System;
+
+namespace TR_RollForward_Dispatcher
+{
+    public class ConnectionsManager
+    {
+        public ConnectionsManager(ICodedWorkflowsServiceContainer resolver)
+        {
+        }
+    }
+}
